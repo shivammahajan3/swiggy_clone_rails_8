009b5c4d1681e27bd9773cd26cd277f693302f07
shivammahajan <shimahajan@bestpeers.in>
2025-03-01 03:01:53 +0530

    adding the devise for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :restros
   has_many :orders
   enum :role, [ :user, :restro_user, :delivery_partner, :admin ]
