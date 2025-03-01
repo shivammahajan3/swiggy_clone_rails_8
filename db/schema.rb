c5c7ae6652be10f34c5261051fd21404cea6d1fa
shivammahajan <shimahajan@bestpeers.in>
2025-03-01 18:16:19 +0530

    adding the product description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_01_064637) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_01_083510) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_01_064637) do
     t.integer "restro_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["restro_id"], name: "index_products_on_restro_id"
   end
 
