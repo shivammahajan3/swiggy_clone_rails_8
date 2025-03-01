45cfe78431269e671a5fc043a0e407f538dd36cd
shivammahajan <shimahajan@bestpeers.in>
2025-03-01 13:51:59 +0530

    adding the new and show restro action and also attach the restro profiles

--- app/controllers/restros_controller.rb
+++ app/controllers/restros_controller.rb
@@ -4,4 +4,31 @@ class RestrosController < ApplicationController
     @restros = Restro.all
   end
 
+  def show
+    @restro = Restro.find(params[:id])
+    @products = @restro.products
+  end
+
+  def new
+    @restro = Restro.new
+  end
+
+  def create
+    @restro = Restro.new(restro_params)
+    @restro.user = User.first
+    if @restro.save
+      flash[:notice] = "Resturent added Successfully"
+      redirect_to restros_path
+    else
+      flash[:alert] = "Something went wrong!"
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def restro_params
+    params.expect(restro: [ :name, :address, :phone_number, :res_profile])
+  end
+
 end
--- app/models/restro.rb
+++ app/models/restro.rb
@@ -2,4 +2,5 @@ class Restro < ApplicationRecord
   belongs_to :user
   has_many :products
   has_many :orders
+  has_one_attached :res_profile
 end
